2e2d1555477203a5065b1fcd0156b6eafe5b82de
Chadd Kiggins <120539962+ChaddKiggins@users.noreply.github.com>
2026-04-21 19:51:26 -0500

    Deleted old thermal model folder and created neew folder for youtube video

--- Thermal Model/main.py
+++
@@ -1,9 +0,0 @@
-import finite_difference as fd
-
-def main():
-    solver = fd.FiniteDifference_1D(40, 0.0105, 700, 1, 4030, 815, 1, 5, 298.15, 300000, 298.15)
-    solver.BackwardEuler()
-    solver.Plot()
-
-if __name__ == "__main__":
-    main()
